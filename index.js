e763a4681f21e474a467acf561493dc25bbec00e
shekhar bansal <bansalshekhar2002@gmail.com>
2023-09-24 03:09:57 +0530

    changes

--- index.js
+++ index.js
@@ -29,12 +29,22 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+// app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors())
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
 app.use("/api/comments",commentRoute)
+
+app.get('/', async (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Credentials","true");
+
+    res.status(200).json({
+      message: 'KEEP BLOGGING',
+    });
+  });
 
 //image upload
 const storage=multer.diskStorage({
